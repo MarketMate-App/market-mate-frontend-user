fbb8fcffca687bac10dc51b2141e95234dcb2184
Gideon Appau <gideonad.codez@gmail.com>
2024-12-11 08:56:40 -0800

    reset

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,5 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { Stack, useRouter } from "expo-router";
-// Import your global CSS file
-import "../global.css";
+import { Stack } from "expo-router";
 
-const RootLayout = () => {
-  const router = useRouter();
-  return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Home",
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="register"
-        options={{
-          headerTitle: "Create Account",
-          headerRight: () => (
-            <Button
-              title="Open Modal"
-              onPress={() => {
-                router.push("/modal");
-              }}
-            />
-          ),
-        }}
-      />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
-};
-
-export default RootLayout;
+export default function RootLayout() {
+  return <Stack />;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,15 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
-import { Link, useRouter } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
 
-const LoginPage = () => {
-  const router = useRouter();
-  const handlePress = () => {
-    router.replace("/(tabs)/home");
-  };
+export default function Index() {
   return (
-    <SafeAreaView className="flex-1 flex-col items-center justify-center p-2">
-      <Pressable onPress={handlePress}>
-        <Text>Go to home</Text>
-      </Pressable>
-      <Link href={"/register"} asChild>
-        <Pressable className=" w-full h-16 bg-[#61ad4e] text-white items-center justify-center rounded-full absolute bottom-0">
-          <Text className="text-[#f9f9f9] font-bold text-base">
-            Create Account
-          </Text>
-        </Pressable>
-      </Link>
-    </SafeAreaView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
   );
-};
-
-export default LoginPage;
+}
