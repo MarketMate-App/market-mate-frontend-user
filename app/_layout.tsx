e3a37970152414c634345ee0c3079dd038f57a1e
Gideon Appau <gideonad.codez@gmail.com>
2024-12-12 08:32:59 -0800

    reset

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
 import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    "Gilroy Regular": require("../assets/fonts/Gilroy-Regular.ttf"),
+    "Gilroy Medium": require("../assets/fonts/Gilroy-Medium.ttf"),
+    "Gilroy Bold": require("../assets/fonts/Gilroy-Bold.ttf"),
+    "Plus Jakarta Sans Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Plus Jakarta Sans Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Plus Jakarta Sans Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { ImageBackground, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ImageBackground
+      source={require("@/assets/images/woman.png")}
+      style={{ width: "100%", height: "100%" }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <LinearGradient
+        colors={["rgba(0,0,0,0.5)", "transparent"]}
+        style={{ flex: 1 }}
+      >
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 32,
+              fontFamily: "Plus Jakarta Sans Bold",
+            }}
+          >
+            Welcome to Expo Router
+          </Text>
+        </View>
+      </LinearGradient>
+    </ImageBackground>
   );
 }
